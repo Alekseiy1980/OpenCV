3df38d2172b4706b46b53c31aa1db21d8fa37609
Алексей <129889765+Alekseiy1980@users.noreply.github.com>
2023-07-16 11:47:37 +0300

    распознование лиц через WebCamera

--- video_webCamera.py
+++ video_webCamera.py
@@ -1,20 +1,35 @@
 import cv2
 
 face_cascades = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml") # подключаем нейросеть работающая с лицами
-img = cv2.imread("faces.jpg")
-img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Меняем цветную картинку на черно-белую
-faces = face_cascades.detectMultiScale(img_gray)
+'''
+Нейросеть работает с картинкой 
+'''
 
-for ( x, y, w, h) in faces:     # перебираем координаты для нахождения лиц и и отрисовки рамок
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+# img = cv2.imread("faces.jpg")
+# img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Меняем цветную картинку на черно-белую
+# faces = face_cascades.detectMultiScale(img_gray)
+#
+# for ( x, y, w, h) in faces:     # перебираем координаты для нахождения лиц и и отрисовки рамок
+#     cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+#
+# cv2.imshow('Result', img) # # Выводим саму картинку( 'Result' -это названия открывающегося окна,img - это наша картинка
+# cv2.waitKey(0)
 
-cv2.imshow('Result', img) # # Выводим саму картинку( 'Result' -это названия открывающегося окна,img - это наша картинка
-cv2.waitKey(0)
-# cap = cv2.VideoCapture(0) # Подключаем вебку(0 - это первая вебка 1 - это вторая вебка) если вебки нет вместо 0 пишим адресс видео как в с img
-#
-# while True:
-#     success, frame = cap.read() # получаем картинку
-#     cv2.imshow('Camera', frame )
-#
-#     if cv2.waitKey(1) & 0xff == ord('q'):
-#         break
+'''
+Нейросеть работает с вебкамерой ( можно подключить видео вместо камеры) 
+'''
+
+cap = cv2.VideoCapture(0) # Подключаем вебку(0 - это первая вебка 1 - это вторая вебка) если вебки нет вместо 0 пишим адресс видео как в с img
+
+while True:
+    success, frame = cap.read() # получаем картинку
+    # cv2.imshow('Camera', frame )
+
+    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #Меняем цветную картинку на черно-белую
+    faces = face_cascades.detectMultiScale(img_gray)
+
+    for ( x, y, w, h) in faces:     # перебираем координаты для нахождения лиц и и отрисовки рамок
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.imshow('Result', frame)  # # Выводим саму картинку( 'Result' -это названия открывающегося окна,img - это наша картинка
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
